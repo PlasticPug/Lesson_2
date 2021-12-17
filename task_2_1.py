6aa8cf17e7c3385a95197e0228f7394c5e2decbd
PlasticPug <plasticpug2019@gmail.com>
2021-12-17 11:40:30 +0300

    Homework_2

---
+++ task_2_1.py
@@ -0,0 +1,13 @@
+# 1. Выяснить тип результата следующих выражений:
+# 15 * 3
+# 15 / 3
+# 15 // 2
+# 15 ** 2
+
+result = 15 * 3
+result2 = 15 / 3
+result3 = 15 // 2
+result4 = 15 ** 2
+
+print(type(result), type(result2), type(result3), type(result4))
+
